a2daf0c6f7ccf6e61291777da5b41ed36d148473
._______166 <retromac9x2@gmail.com>
2024-11-27 19:31:56 +0000

    remove dropdown menu
    it does not work

--- assets/js/material-kit.js
+++ assets/js/material-kit.js
@@ -151,19 +151,3 @@ $(document).on('click', '.navbar-toggler', function() {
 })(jQuery, document, Math);
 
 $.ripple(".btn, .nav-item");
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function dropdown1Function() {
-  document.getElementById("dropdown1").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("dropdown1");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
